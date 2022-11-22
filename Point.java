5ea584ed392490747a61add63e922437d340ddc1
Yangjianxiao0203 <112916147+Yangjianxiao0203@users.noreply.github.com>
2022-11-22 13:15:22 -0500

    Add files via upload

--- Point.java
+++ Point.java
@@ -1,13 +1,28 @@
-public class Point {
+import java.util.ArrayList;
+
+public class Point implements Comparable<Point>{
     private double x;
     private double y;
+    public boolean judge=false; // false -> x , true -> y
+    public int depth;
+    public Point left=null;
+    public Point right=null;
+    public Point father=null;
+    public ArrayList<Point> leftChilds;
+    public ArrayList<Point> rightChilds;
     public Point(double x,double y){
         this.x=x;
         this.y=y;
+        this.depth=0;
+        this.leftChilds=new ArrayList<>();
+        this.rightChilds=new ArrayList<>();
     }
     public Point(){
         this.x=0;
         this.y=0;
+        this.depth=0;
+        this.leftChilds=new ArrayList<>();
+        this.rightChilds=new ArrayList<>();
     }
     public double getX(){
         return x;
@@ -22,24 +37,26 @@ public class Point {
         double R=6371;
         double phi1=x*Math.PI/180;
         double phi2= x_other*Math.PI/180;
-        double delta_phi=(phi2-phi1)*Math.PI/180;
+        double delta_phi=(phi2-phi1);
         double delta_lamda=(y_other-y)*Math.PI/180;
         double a=Math.sin(delta_phi/2)*Math.sin(delta_phi/2)+Math.cos(phi1)*Math.cos(phi2)*Math.sin(delta_lamda)*Math.sin(delta_lamda);
         double c=2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
 
         return R*c;
     }
-}
 
-//const R = 6371e3; // metres
-//        const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-//        const φ2 = lat2 * Math.PI/180;
-//        const Δφ = (lat2-lat1) * Math.PI/180;
-//        const Δλ = (lon2-lon1) * Math.PI/180;
-//
-//        const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-//        Math.cos(φ1) * Math.cos(φ2) *
-//        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-//        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-//
-//        const d = R * c; // in metres
+
+    @Override
+    public int compareTo(Point o) {
+        if(judge) {
+            if(this.getY()>o.getY()) {return 1;}
+            else if(this.getY()==o.getY()) {return 0;}
+            else {return -1;}
+        }
+        else {
+            if(this.getX()>o.getX()) {return 1;}
+            else if(this.getX()==o.getX()) {return 0;}
+            else {return -1;}
+        }
+    }
+}
